2af5b9f4dd1b0f06d1f10f8bb20ad11a17d79110
yjqiang <8900942+yjqiang@users.noreply.github.com>
2021-05-06 16:11:04 +0800

    大整数会有精度损失
    https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/zhihu/zhihu_plus.js

--- dev/zhihu/zhihu_topstory_recommend.js
+++ dev/zhihu/zhihu_topstory_recommend.js
@@ -9,6 +9,7 @@ const main = async() => {
 
     // START
     let body = $response.body;
+    json = json.replace(/("id")\s*:\s*(\d{15,})/g, "\$1:\"$2\"");  // 大整数会有精度损失
     body = JSON.parse(body);
     body['data'] = body['data'].filter(element => !(element.hasOwnProperty('ad')));
     body = JSON.stringify(body);
