ceca583b60bae27e321b89f72c45b5f0b747842a
yjqiang <8900942+yjqiang@users.noreply.github.com>
2021-10-12 18:51:46 +0800

    根据最新数据更新 b 站 tab 数据

--- dev/bilibili/bilibili_tab.js
+++ dev/bilibili/bilibili_tab.js
@@ -35,30 +35,30 @@ const main = async() => {
             id: 41,
             name: '热门',
             uri: 'bilibili://pegasus/hottopic',
-            tab_id: '热门tab',
+            tab_id: 'hottopic',
             pos: 3
         },
         {
             id: 42,
             name: '追番',
             uri: 'bilibili://pgc/home',
-            tab_id: '追番Tab',
+            tab_id: 'bangumi',
             pos: 4
         },
         {
-            "id": 151,
-            "name": "影视",
-            "uri": "bilibili://pgc/cinema-tab",
-            "tab_id": "影视tab",
-            "pos": 5,
+            id: 151,
+            name: '影视',
+            uri: 'bilibili://pgc/cinema-tab',
+            tab_id: 'film',
+            pos: 5
         },
     ]
 
     // 首页右上角的 tab 栏（删除了游戏中心栏目）
     body['data']['top'] = [
         {
-            id: 48,
-            icon: 'http://i0.hdslb.com/bfs/archive/ada7b340bddee80b3e956c8bfd844463e8e08d48.png',
+            id: 176,
+            icon: 'http://i0.hdslb.com/bfs/archive/d43047538e72c9ed8fd8e4e34415fbe3a4f632cb.png',
             name: '消息',
             uri: 'bilibili://link/im_home',
             tab_id: '消息Top',
@@ -67,38 +67,38 @@ const main = async() => {
     ]
 
     // app 是下边 tab 栏（删除了会员购栏目）
-    body['data']['bottom'] = [
+    body['data']['bottom'] =[
         {
-            id: 43,
-            icon: 'http://i0.hdslb.com/bfs/archive/1ab5459ccb18c7a996315327257375be3da19886.png',
-            icon_selected: 'http://i0.hdslb.com/bfs/archive/d6a45f06684562dd9cb6914007658c0cdb17bbff.png',
+            id: 177,
+            icon: 'http://i0.hdslb.com/bfs/archive/63d7ee88d471786c1af45af86e8cb7f607edf91b.png',
+            icon_selected: 'http://i0.hdslb.com/bfs/archive/e5106aa688dc729e7f0eafcbb80317feb54a43bd.png',
             name: '首页',
             uri: 'bilibili://main/home/',
-            tab_id: '首页Bottom',
+            tab_id: 'home',
             pos: 1
         },
         {
-            id: 44,
-            icon: 'http://i0.hdslb.com/bfs/archive/b4f621f268c1f9eda501805135f132aa9498b0ba.png',
-            icon_selected: 'http://i0.hdslb.com/bfs/archive/94539249e59621214f7dc1226cf38a2b8fe4c64f.png',
+            id: 178,
+            icon: 'http://i0.hdslb.com/bfs/archive/9c453a54eb83f5140cd098bf2e8ed8a599edc7fe.png',
+            icon_selected: 'http://i0.hdslb.com/bfs/archive/79d29e6ac3b6e52652881b050e63988e2038130f.png',
             name: '频道',
             uri: 'bilibili://pegasus/channel/',
             tab_id: '频道Bottom',
             pos: 2
         },
         {
-            id: 45,
-            icon: 'http://i0.hdslb.com/bfs/archive/0f15d5f5be25af29eec6f002561d5000a77cc914.png',
-            icon_selected: 'http://i0.hdslb.com/bfs/archive/1d37925562cd3e7d2e5f0868f966b5b9a8b86cde.png',
+            id: 179,
+            icon: 'http://i0.hdslb.com/bfs/archive/86dfbe5fa32f11a8588b9ae0fccb77d3c27cedf6.png',
+            icon_selected: 'http://i0.hdslb.com/bfs/archive/25b658e1f6b6da57eecba328556101dbdcb4b53f.png',
             name: '动态',
             uri: 'bilibili://following/home/',
-            tab_id: '动态Bottom',
+            tab_id: 'dynamic',
             pos: 3
         },
         {
-            id: 49,
-            icon: 'http://i0.hdslb.com/bfs/archive/aafe71f10eeb5086ac119e4dad769c5aad4d86a2.png',
-            icon_selected: 'http://i0.hdslb.com/bfs/archive/36e080bbd8ae858af664ef251741124e04241942.png',
+            id: 181,
+            icon: 'http://i0.hdslb.com/bfs/archive/4b0b2c49ffeb4f0c2e6a4cceebeef0aab1c53fe1.png',
+            icon_selected: 'http://i0.hdslb.com/bfs/archive/a54a8009116cb896e64ef14dcf50e5cade401e00.png',
             name: '我的',
             uri: 'bilibili://user_center/',
             tab_id: '我的Bottom',
