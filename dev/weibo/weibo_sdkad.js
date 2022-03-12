7c5b78a2bbaeea5a1c853d419da4fa11ce20e8d3
yjqiang <8900942+yjqiang@users.noreply.github.com>
2022-03-13 00:55:47 +0800

    close #8

--- dev/weibo/weibo_sdkad.js
+++ dev/weibo/weibo_sdkad.js
@@ -1,19 +1,25 @@
 import { $done } from "../../runtime/surge/public.js"
-import { async_request, read_request_json_file } from "../../runtime/utils.js"
+import {read_text_file} from "../../runtime/utils.js"
 
 const main = async() => {
-    let request = read_request_json_file('../../requests_and_responses/requests/jsons/weibo/weibo_sdkad.json');
-    // console.log(request);
-
-    let $response = await async_request(request.method, request.url, request.data, request.headers);
-    console.log($response);
+    let $response = {
+        // 这货原文是 json + OK，我存到文件数据是 json，还原回原来请求返回的数据罢了
+        body: read_text_file('../../requests_and_responses/responses/private/weibo_sdkad.json').replace(/\r?\n/g, "") + "OK"  // https://stackoverflow.com/a/21895354)
+    };
 
     // START
     let body = JSON.parse($response.body.match(/({.*})OK/)[1]);  // $response.body： {json..,}OK
 
-    body['ads'] = [];
+    for (let item of body['ads']) {
+        // console.log(`${item['begintime']} -- ${item['endtime']}`);
+        item['displaytime'] = 0;  // 显示时间
+        // 2040 年
+        item['begintime'] = '2040-12-27 00:00:01';
+        item['endtime'] = '2040-12-27 23:59:59';
+    }
 
     $done({body: `${JSON.stringify(body)}OK`});
+
     // END
 };
 
--- scripts/weibo/weibo_sdkad.js
+++ scripts/weibo/weibo_sdkad.js
@@ -1,3 +1,9 @@
 let body = JSON.parse($response.body.match(/({.*})OK/)[1]);  // $response.body： {json..,}OK
-body['ads'] = [];
+for (let item of body['ads']) {
+    // console.log(`${item['begintime']} -- ${item['endtime']}`);
+    item['displaytime'] = 0;  // 显示时间
+    // 2040 年
+    item['begintime'] = '2040-12-27 00:00:01';
+    item['endtime'] = '2040-12-27 23:59:59';
+}
 $done({body: `${JSON.stringify(body)}OK`});
