587ff75403ac7f1289abfe0c19f1780a549ce4a3
yjqiang <8900942+yjqiang@users.noreply.github.com>
2022-06-24 23:41:19 +0800

    去广告

--- dev/weibo/weibo_sdkad.js
+++ dev/weibo/weibo_sdkad.js
@@ -10,6 +10,9 @@ const main = async() => {
     // START
     let body = JSON.parse($response.body.match(/({.*})OK/)[1]);  // $response.body： {json..,}OK
 
+    if (body.background_delay_display_time)
+        body.background_delay_display_time = 60*60*24*365;
+
     for (let item of body['ads']) {
         // console.log(`${item['begintime']} -- ${item['endtime']}`);
         item['displaytime'] = 0;  // 显示时间
