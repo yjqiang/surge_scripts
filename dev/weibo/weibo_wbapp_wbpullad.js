517039544b9b6273b20b1c8c588da406b288cdf3
yjqiang <8900942+yjqiang@users.noreply.github.com>
2023-05-14 05:18:01 +0800

    fix

--- dev/weibo/weibo_wbapp_wbpullad.js
+++ dev/weibo/weibo_wbapp_wbpullad.js
@@ -9,11 +9,16 @@ const main = async() => {
     // START
     let body = JSON.parse($response.body);
 
-    // https://raw.githubusercontent.com/tielog/AD/master/wb_launch.js
     if (body.hasOwnProperty('cached_ad') && body['cached_ad'].hasOwnProperty('ads'))
-        body['cached_ad']['ads'] = [];
+    for (let item of body['cached_ad']['ads']) {
+        item['duration'] = 0
 
+        // 2026-11-30 15:48:24
+        item['end_date'] = '1796024904'
+        item['start_date'] = '1796024914'
+    }
     $done({body: JSON.stringify(body)});
+
     // END
 };
 
