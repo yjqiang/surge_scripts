fc72c9a4c5f054f8864cdb71cd9fcae5ae974729
yjqiang <8900942+yjqiang@users.noreply.github.com>
2023-06-29 02:27:17 +0800

    微博下面，评论之上的贴片广告

--- dev/weibo/weibo_statuses_extend.js
+++ dev/weibo/weibo_statuses_extend.js
@@ -3,18 +3,20 @@ import { read_text_file } from "../../runtime/utils.js"
 
 const main = async() => {
     let $response = {
-        body: read_text_file('../../requests_and_responses/responses/private/weibo_statuses_extend1.json')
+        body: read_text_file('../../requests_and_responses/responses/private/weibo_statuses_extend2.json')
     };
 
     // START
     let body = JSON.parse($response.body);
 
-    if (body.hasOwnProperty('trend')) {
+    if (body.hasOwnProperty('trend'))
         delete body['trend'];
-        $done({body: JSON.stringify(body)});
-    }
-    else
-        $done({});
+
+    // 微博下面，评论之上的贴片广告
+    if (body.hasOwnProperty('head_cards'))
+        body['head_cards'] = body['head_cards'].filter(element => !(element.hasOwnProperty("actionlog") && element['actionlog']['source'] === "ad"));
+
+    $done({body: JSON.stringify(body)});
 
 
     // END
--- scripts/weibo/weibo_statuses_extend.js
+++ scripts/weibo/weibo_statuses_extend.js
@@ -1,7 +1,7 @@
 let body = JSON.parse($response.body);
-if (body.hasOwnProperty('trend')) {
+if (body.hasOwnProperty('trend'))
     delete body['trend'];
-    $done({body: JSON.stringify(body)});
-}
-else
-    $done({});
+// 微博下面，评论之上的贴片广告
+if (body.hasOwnProperty('head_cards'))
+    body['head_cards'] = body['head_cards'].filter(element => !(element.hasOwnProperty("actionlog") && element['actionlog']['source'] === "ad"));
+$done({body: JSON.stringify(body)});
