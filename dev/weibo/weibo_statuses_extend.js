1b1ddfd1e208551cd71edf61785caf21fc837263
github_yjqiang <8900942+yjqiang@users.noreply.github.com>
2023-07-11 16:36:03 +0800

    nmb

--- dev/weibo/weibo_statuses_extend.js
+++ dev/weibo/weibo_statuses_extend.js
@@ -14,7 +14,7 @@ const main = async() => {
 
     // 微博下面，评论之上的贴片广告
     if (body.hasOwnProperty('head_cards'))
-        body['head_cards'] = body['head_cards'].filter(element => !(element.hasOwnProperty("actionlog") && element['actionlog']['source'].include("ad")));
+        body['head_cards'] = body['head_cards'].filter(element => !(element.hasOwnProperty("actionlog") && element['actionlog']['source'] === "ad" || element.hasOwnProperty("action_button") && element['action_button'].hasOwnProperty("actionlog") && element['action_button']['actionlog']['source'] === "ad"));
 
     $done({body: JSON.stringify(body)});
 
--- scripts/weibo/weibo_statuses_extend.js
+++ scripts/weibo/weibo_statuses_extend.js
@@ -3,5 +3,5 @@ if (body.hasOwnProperty('trend'))
     delete body['trend'];
 // 微博下面，评论之上的贴片广告
 if (body.hasOwnProperty('head_cards'))
-    body['head_cards'] = body['head_cards'].filter(element => !(element.hasOwnProperty("actionlog") && element['actionlog']['source'].include("ad")));
+    body['head_cards'] = body['head_cards'].filter(element => !(element.hasOwnProperty("actionlog") && element['actionlog']['source'] === "ad" || element.hasOwnProperty("action_button") && element['action_button'].hasOwnProperty("actionlog") && element['action_button']['actionlog']['source'] === "ad"));
 $done({body: JSON.stringify(body)});
