e7204e5120a4e029ce9a8ed5b898822e288338a2
github_yjqiang <8900942+yjqiang@users.noreply.github.com>
2025-08-23 14:59:35 +0800

    fix bug

--- dev/weibo/weibo_cardlist.js
+++ dev/weibo/weibo_cardlist.js
@@ -13,7 +13,7 @@ const main = async() => {
     if (body['cards'])  // 包括 body['cards'] 可能为 null 的情况
         body['cards'] = body['cards'].filter(element => !(element['card_type'] === 9 && element.hasOwnProperty('mblog') && element['mblog'].hasOwnProperty('promotion') && element['mblog']['promotion']['type'] === 'ad'));
     if (body['items'])
-        body['items'] = body['items'].filter(element => !(element.hasOwnProperty('data') && elent['data']['is_ad'] === 1));
+        body['items'] = body['items'].filter(element => !(element.hasOwnProperty('data') && element['data']['is_ad'] === 1));
     $done({body: JSON.stringify(body)});
     // END
 };
--- scripts/weibo/weibo_cardlist.js
+++ scripts/weibo/weibo_cardlist.js
@@ -3,5 +3,5 @@ let body = JSON.parse($response.body);
 if (body['cards'])  // 包括 body['cards'] 可能为 null 的情况
     body['cards'] = body['cards'].filter(element => !(element['card_type'] === 9 && element.hasOwnProperty('mblog') && element['mblog'].hasOwnProperty('promotion') && element['mblog']['promotion']['type'] === 'ad'));
 if (body['items'])
-    body['items'] = body['items'].filter(element => !(element.hasOwnProperty('data') && elent['data']['is_ad'] === 1)); 
+    body['items'] = body['items'].filter(element => !(element.hasOwnProperty('data') && element['data']['is_ad'] === 1)); 
 $done({body: JSON.stringify(body)});
